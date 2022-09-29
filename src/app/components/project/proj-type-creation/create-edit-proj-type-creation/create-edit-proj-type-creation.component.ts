c56c2128576b26accf41a868869f34b799023185
ganeshfromindia <ganeshghate@hotmail.com>
2022-09-29 17:52:11 +0530

    UI Fixes

--- src/app/components/project/proj-type-creation/create-edit-proj-type-creation/create-edit-proj-type-creation.component.ts
+++ src/app/components/project/proj-type-creation/create-edit-proj-type-creation/create-edit-proj-type-creation.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-create-edit-proj-type-creation',
@@ -7,9 +9,57 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CreateEditProjTypeCreationComponent implements OnInit {
 
-  constructor() { }
+  public projTypeId: string = '';
+  public projTypeCreateEditForm: FormGroup;
 
-  ngOnInit(): void {
+  constructor(private route: ActivatedRoute, private _fb: FormBuilder) { 
+    this.projTypeCreateEditForm = this._fb.group({
+      projTypeName:['', Validators.required],
+      capex:['', Validators.required],
+      projHierarchy:['', Validators.required],
+      additionalField1:['', Validators.required],
+      additionalField2:['', Validators.required]
+    })
   }
 
+  ngOnInit(): void {
+    this.projTypeId = this.route.snapshot.params['id'];
+    if(this.projTypeId) {
+    //  this.urlHttpParams = {
+    //    companyName: '',
+    //    adminEmailId: '',
+    //    id: this.value
+    //  };  
+    //  this._beService
+    //       .getMethod(
+    //         'get/company-list?',
+    //         page,
+    //         pageSize,
+    //         urlHttpParams
+    //       )
+    //       .subscribe({
+    //   next: data => {
+    //     console.log(data);
+    //   },
+    //   error: e => {
+    //     console.log(e);
+    //   }
+    // })
+      let x = {
+        projTypeName: 'Large Capex Project',
+        capex: 'More than 200 Crore',
+        projHierarchy: 'Big Project Hierarchy',
+        additionalField1: 'Investors Involved',
+        additionalField2: 'Technology Status',
+      }
+      this.projTypeCreateEditForm.patchValue(x)
+    }
+
+  }
+
+  onSubmitProjTypeCreateEdit() {
+    const formData = this.projTypeCreateEditForm.getRawValue();
+    console.log(formData)
+
+  }
 }
