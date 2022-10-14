9e040293ccec185b2036cadbe38159447b2cea57
ganeshfromindia <ganeshghate@hotmail.com>
2022-10-14 13:07:52 +0530

    Upload Doc HTML CSS

--- src/app/components/document/upload-document/upload-document.component.ts
+++ src/app/components/document/upload-document/upload-document.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormArray, FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { FormArray, FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { BackendService } from 'src/app/services/backend.service';
 import { UserService } from 'src/app/services/user.service';
@@ -14,32 +14,39 @@ export class UploadDocumentComponent implements OnInit {
   urlHttpParams: any = {};
   tags: any[] = [];
   docTypes: any[] = [];
+  additionalRights: any[] = [];
   showError: string = '';
   documentUploadForm: FormGroup;
 
   constructor(private router: Router, private _beService: BackendService, private userService: UserService, private _fb: FormBuilder) {
     this.documentUploadForm = this._fb.group({
       docType:['1', Validators.required],
-      tags:this._fb.array([])
+      additionalRights :[null, Validators.required],
+      taggingHead :this._fb.array([])
     })
   }
 
   ngOnInit(): void {
     this.setTaggingData(1, 10);
     this.setDocTypes(1, 10);
-    this.docTypes = [
-      {id: 1, docType: 'PDF'},
-      {id: 2, docType: 'WORD'},
-      {id: 3, docType: 'EXCEL'}
+    this.additionalRights = [
+      {id: 1, right: 'Within Company'},
+      {id: 2, right: 'Across Department'},
+      {id: 3, right: 'Sub Department'},
+      {id: 4, right: 'Within Company'},
+      {id: 5, right: 'Across Department'},
+      {id: 6, right: 'Sub Department'},
+      {id: 7, right: 'Within Company'},
+      {id: 8, right: 'Across Department'},
+      {id: 9, right: 'Sub Department'}
     ]
   }
 
   async setDocTypes(page: number, pageSize: number) {
-    return
     this.docTypes = []
-    let url = 'get/designation-list?';
+    let url = 'common/get/document/type?';
     this.urlHttpParams = {
-      companyId: this.userService.getCompanyID()    
+      adminId : this.userService.getUserId()    
     };
     let returnedAlerts: any = await this.setData(page, pageSize, url, this.urlHttpParams);
     if(returnedAlerts.flag) {
@@ -77,10 +84,6 @@ export class UploadDocumentComponent implements OnInit {
       },5000)
     } else {
       if(returnedAlerts.data.status == 200) this.tags = returnedAlerts.data.payLoad;
-      const tagsArray = this.documentUploadForm.get('tags') as FormArray;
-      this.tags.forEach((data) => {
-        tagsArray.push(this.buildTagsForm(data))
-      })
     }
   }
 
@@ -112,17 +115,14 @@ export class UploadDocumentComponent implements OnInit {
     }) 
   }
 
-  buildTagsForm(item: any) {
-    return this._fb.group({
-      id: item.id,
-      taggingHeadName: item.taggingHeadName
-    })
-  }
+  onChange(id: string, data: any) {
+    const tagsArray: FormArray = this.documentUploadForm.get('taggingHead') as FormArray;
 
-  setTagForm(data: any) {
-    console.log(data)
-    if(data.target.checked) {
-      
+    if (data.target.checked) {
+      tagsArray.push(new FormControl(id));
+    } else {
+      let index = tagsArray.controls.findIndex(x => x.value == id)
+      tagsArray.removeAt(index);
     }
   }
 
