76210433dceb3ca41fef8b64dc6167f29a19674b
Aparna [brandlumin] <88875925+raiaparna@users.noreply.github.com>
2022-10-27 18:58:18 +0530

    Integrating LocationStrategy & HashLocationStrategy

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,38 +1,30 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { RoutingComponents } from './app-routing.module';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
-import { HttpInterceptorService } from './services/http-interceptor.service';
-import { LoginActivateGuard } from './services/login-activate.guard';
-import { AuthActivateGuard } from './services/auth-activate.guard';
-
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { RoutingComponents } from "./app-routing.module";
+import { NgbModule } from "@ng-bootstrap/ng-bootstrap";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+import { HttpClientModule, HTTP_INTERCEPTORS } from "@angular/common/http";
+import { HttpInterceptorService } from "./services/http-interceptor.service";
+import { LoginActivateGuard } from "./services/login-activate.guard";
+import { AuthActivateGuard } from "./services/auth-activate.guard";
+import { LocationStrategy, HashLocationStrategy } from "@angular/common";
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    RoutingComponents
+  declarations: [AppComponent, RoutingComponents],
+  imports: [BrowserModule, AppRoutingModule, NgbModule, ReactiveFormsModule, FormsModule, HttpClientModule],
+  providers: [
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: HttpInterceptorService,
+      multi: true,
+    },
+    { provide: LocationStrategy, useClass: HashLocationStrategy },
+    LoginActivateGuard,
+    AuthActivateGuard,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    NgbModule,
-    ReactiveFormsModule,
-    FormsModule,
-    HttpClientModule
-  ],
-  providers: [{
-    provide: HTTP_INTERCEPTORS,
-    useClass: HttpInterceptorService,
-    multi: true,
-  },
-  LoginActivateGuard,
-  AuthActivateGuard 
-  ],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
